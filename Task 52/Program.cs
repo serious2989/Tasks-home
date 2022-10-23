63bc412fd540646bd0d0a761b8cacec305bc0eda
Dmitriy Vasilyev <serious2989@gmail.com>
2022-10-23 17:19:24 +0300

    task 52

---
+++ Task 52/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(10, 200);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+string ArithmeticMean(int[,] array)
+{
+    string result = string.Empty;
+    for (int n = 0; n < array.GetLength(1); n++)
+    {
+        int sum = 0;
+        int count = 0;
+        double mean = 0;
+        for (int m = 0; m < array.GetLength(0); m++)
+        {
+            sum += array[m, n];
+            count++;
+        }
+        mean = sum / count;
+        result += mean.ToString() + " ";
+    }
+    return result;
+}
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(ArithmeticMean(array));
+
